9ebb1bdbc8df4754314ccd5750d5922b40c8bc2c
Dev <dev@example.com>
2026-02-24 16:45:38 -0300

    Fix board locking and double click bug

--- board.js
+++ board.js
@@ -11,65 +11,73 @@ export default class Board {
     second = 0
     cron
     storage = []
+    lockBoard = false
 
-    constructor(boardId){
-        this.board = boardId; 
+    constructor(boardId) {
+        this.board = boardId;
     }
 
-    create = () =>{   
+    create = () => {
         let startButtin = document.querySelector('#continue')
         let modal = document.querySelector('#modal-bg');
-        startButtin.addEventListener('click', () =>{
+        startButtin.addEventListener('click', () => {
             modal.style.display = 'none'
             this.start()
-        })     
-        for(let i=0; i<Figures.number; i++){
+        })
+        for (let i = 0; i < Figures.number; i++) {
             this.tmpBoard.push(Figures.get(i)) //10 primeiras cartas
             this.tmpBoard.push(Figures.get(i)) //10 cartas repetidas
         }
 
         this.tmpBoard = this.shuffle(this.tmpBoard);
-          console.log(this.tmpBoard)
+        console.log(this.tmpBoard)
     }
     shuffle = (array) => {
-        let currentIndex = array.length,  randomIndex;
-        
+        let currentIndex = array.length, randomIndex;
+
         // While there remain elements to shuffle.
         while (currentIndex != 0) {
-        
+
             // Pick a remaining element.
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-        
+
             // And swap it with the current element.
             [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+                array[randomIndex], array[currentIndex]];
         }
         return array;
     }
-        
-    handleClick = (event) =>{
+
+    handleClick = (event) => {
+        if (this.lockBoard) return;
         const square = event.target
+        if (square.classList.contains('flip')) return;
+
         const position = square.id
-        if(!this.firstCard){
+        if (!this.firstCard) {
             this.firstCard = this.tmpBoard[position]
             square.classList.add('flip')
             this.twoCards.push(square)
             square.style.backgroundImage = 'none'
         }
-        
-        else if(!this.secondCard){
+
+        else if (!this.secondCard) {
             this.secondCard = this.tmpBoard[position]
             square.classList.add('flip')
             this.twoCards.push(square)
+            this.lockBoard = true;
             const sameCard = this.checkCard()
             square.style.backgroundImage = 'none'
-            if(sameCard){
+            if (sameCard) {
                 this.twoCards = []
                 console.log('acertou');
-            }else{
+                this.firstCard = null
+                this.secondCard = null
+                this.lockBoard = false;
+            } else {
                 console.log('errou');
-                
+
                 console.log(this.twoCards);
                 setTimeout(() => {
                     this.twoCards.forEach(card => {
@@ -78,37 +86,38 @@ export default class Board {
                         card.style.backgroundImage = 'url(./images/innovation.png)'
                     })
                     this.twoCards = []
-                },200)
+                    this.firstCard = null
+                    this.secondCard = null
+                    this.lockBoard = false;
+                }, 500)
             }
-            this.firstCard = null
-            this.secondCard = null
         }
-        
-        setTimeout(() =>{
+
+        setTimeout(() => {
             square.innerHTML = `<img class="imageSquare" src="${this.tmpBoard[position].picture}">`
-            if(this.gameOver()){
+            if (this.gameOver()) {
                 this.pause()
-                if(this.pause){
+                if (this.pause) {
                     this.record()
                     this.restartGame()
                 }
             }
-        },100)
-        
+        }, 100)
+
     }
 
     checkCard = () => {
         if (!this.firstCard || !this.secondCard) {
             return false;
         }
-        return this.firstCard.picture === this.secondCard.picture 
+        return this.firstCard.picture === this.secondCard.picture
     }
 
-    figureActually = () =>{
-        document.addEventListener('DOMContentLoaded', ()=>{
-            let squares = document.querySelectorAll('.square');         
-            squares.forEach((square)=>{   
-                square.addEventListener('click', this.handleClick) 
+    figureActually = () => {
+        document.addEventListener('DOMContentLoaded', () => {
+            let squares = document.querySelectorAll('.square');
+            squares.forEach((square) => {
+                square.addEventListener('click', this.handleClick)
             })
         })
     }
@@ -123,7 +132,7 @@ export default class Board {
         let restart = document.querySelector('#restart')
         let name = document.querySelector('#name')
         modal2.style.display = 'flex'
-        if(this.restartGame){
+        if (this.restartGame) {
             nice.innerText = `Parabéns ${String(name.value)}, agora bata seu recorde!`
             restart.addEventListener('click', () => {
                 modal2.style.display = 'none'
@@ -143,10 +152,10 @@ export default class Board {
     record = () => {
         this.storage.push(this.minute)
         this.storage.push(this.second)
-        if(this.minute < this.storage[0] || this.minute == this.storage[0]){
+        if (this.minute < this.storage[0] || this.minute == this.storage[0]) {
             document.querySelector('#minuteR').innerHTML = `${String(this.minute)}m`
         }
-        if(this.second < this.storage[1] || this.second == this.storage[1]){
+        if (this.second < this.storage[1] || this.second == this.storage[1]) {
             document.querySelector('#secondR').innerHTML = `${String(this.second)}s`
         }
     }
@@ -154,7 +163,7 @@ export default class Board {
     start = () => {
         this.cron = setInterval(() => this.timer(), 1000)
     }
-      
+
     timer = () => {
         this.second += 1
         console.log(this.second);
@@ -172,14 +181,14 @@ export default class Board {
     }
 
     reset = () => {
-      this.minute = 0
-      this.second = 0
-      document.getElementById('minute').innerText = '00';
-      document.getElementById('second').innerText = '00';
+        this.minute = 0
+        this.second = 0
+        document.getElementById('minute').innerText = '00';
+        document.getElementById('second').innerText = '00';
     }
 
     pause = () => {
         clearInterval(this.cron)
     }
-        
+
 }
